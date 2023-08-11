a076346bebc43e2ed95d4d8ac7f44e4649e4ffdf
Trevorambler <trevor.ambler@bartlettroofs.com>
2023-08-11 10:52:07 -0600

    Start of L11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,22 @@
 <template>
   <div class="home">
-    <p>My name is {{ name }} my age is {{age }}</p>
-    <button @click="handleClick">Click Me</button>
-    <button @click="age++">add 1 to age </button>
-    <input type="text" v-model="name">
+  <h1>home</h1>
+
+   
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 
-// @ is an alias to /src
-
 
 export default {
   name: 'Home',
   setup() {
-
-   // const p = ref(null)
-
-    const name = ref('mario')
-    const age = ref(30)
     
+   
 
-    const handleClick = () => {
-      name.value = 'luigi'
-      age.value = 35
-    }
-
-    return { name, age, handleClick,}
-    
+     return { }
   }
 }
 </script>
